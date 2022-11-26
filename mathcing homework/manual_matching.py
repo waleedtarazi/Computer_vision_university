761b6f2bd7851d583636f517453be83dbabf7473
waleed tarazi <waleedaltarazy@gmail.com>
2022-11-26 18:29:52 +0300

    nothing changes i think

--- mathcing homework/manual_matching.py
+++ mathcing homework/manual_matching.py
@@ -1,7 +1,7 @@
 import cv2 
 
 template_img = cv2.imread('cup.jpg')
-test_img = cv2.imread('super_side.jpg')
+test_img = cv2.imread('hidden.jpg')
 
 template_img_bw = cv2.cvtColor(template_img,cv2.COLOR_BGR2GRAY)
 test_img_bw = cv2.cvtColor(test_img, cv2.COLOR_BGR2GRAY)
@@ -15,9 +15,9 @@ matcher = cv2.BFMatcher()
 matches = matcher.match(templateDescriptors,testDescriptors)
   
 reslut_img = cv2.drawMatches(template_img, templateKeypoints,
-test_img, testKeypoints, matches[:20],None)
+test_img, testKeypoints, matches[:30],None)
   
-reslut_img = cv2.resize(reslut_img, (1080,1020))
+reslut_img = cv2.resize(reslut_img, (1080,720))
  
 cv2.imshow("matching results", reslut_img)
 cv2.waitKey(0)
